29e0ec265731b98a882aa51945325e3d26315e0c
kramiv <57569429+kramiv@users.noreply.github.com>
2022-02-18 09:51:27 -0600

    Delete SpringExtractFiles/Section2/in28minutes_demo directory

--- SpringExtractFiles/Section2/in28minutes_demo/src/main/java/com/in28minutes_springboot/in28minutes_demo/SortAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.in28minutes_springboot.in28minutes_demo;
-
-public interface SortAlgorithm {//note: "interface"
-	public int[] sort(int[] numbers);//this isn't a method like I've seen before
-}
